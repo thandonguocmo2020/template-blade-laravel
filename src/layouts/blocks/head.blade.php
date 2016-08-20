4b73f559b15f4780023f74a06288e4f38e31fae8
thandonguocmo2020 <thandonguocmo2020@users.noreply.github.com>
2016-08-20 15:27:58 +0700

    Add files via upload

--- src/layouts/blocks/head.blade.php
+++ src/layouts/blocks/head.blade.php
@@ -1,7 +1,7 @@
 @section("head")
 <meta charset="UTF-8">
 <title></title>
-
+<link href="{{ url("/public") }}/library/botstrap/bootstrap.min.css" rel="stylesheet" type="text/css"/>
 <script src="{{ url("/public") }}/library/botstrap/bootstrap.min.js" type="text/javascript"></script>
 <script src="{{ url("/public") }}/library/jquery/jquery.1.9.2.min.js" type="text/javascript"></script>
 <link href="{{ url("/public") }}/library/database-table/datatables.min.css" rel="stylesheet" type="text/css"/>
